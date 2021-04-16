a62adbc18fb51f7e0679bdf6b30375a87ea32785
remya <nairkremya@gmail.com>
2021-04-16 10:58:56 -0400

    swap commit

---
+++ swap.java
@@ -0,0 +1,26 @@
+public class swap {
+
+
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int temp;
+        
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+
+        int x=7;
+        int y=8;
+         x=x+y;
+         y=x-y;
+         x=x-y;
+        System.out.println(x);
+        System.out.println(y);
+
+        
+    }
+    
+}
